c0776827aa8f9ac0a4557e34ad32e88435889b71
boughou1 <boughou1@illinois.edu>
2018-04-04 15:06:06 -0500

    initial docstrings

---
+++ group_keywords.py
@@ -0,0 +1,14 @@
+
+""" Returns a dictionary of {keyword: neighborhood word distribution}
+
+"""
+def neighborhood(input_file, word, M):
+
+	return
+
+
+""" Returns a list of sets of keyword and phrases
+Each set in the list describes one concept
+"""
+def clusterKeywords(input_dists, T):
+	return
